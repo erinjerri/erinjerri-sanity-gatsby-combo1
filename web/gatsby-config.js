c86b2bc6f9527d083d95a680b80449ec07dfe757
Erin <erin@erinjerri.com>
2019-11-15 14:15:25 -0800

    updates

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -10,10 +10,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId,
-        dataset,
-        // To enable preview of drafts, copy .env-example into .env,
-        // and add a token with read permissions
+        projectId: 'bxk74byr',
+        dataset: 'production',
         token: process.env.SANITY_TOKEN,
         watchMode: true,
         overlayDrafts: true
